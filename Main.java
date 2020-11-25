f21ef8b2620cd8d724ff81c437d9a1b284ab3ba0
AidanBugler <buglaida982@gotvdsb.ca>
2020-11-25 13:29:38 +0000

    FIXED TO A FOR LOOP

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    //VAR
+    //LOOPS
+  for (int i=0; i<=20; i+=3)
+  {
+    System.out.println(i);
   }
+}
 }
